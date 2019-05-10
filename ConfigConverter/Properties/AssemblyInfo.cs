94785a77e68aa6f082a2c38afa31260345dd8760
kevL <k3v1s73n@gmail.com>
2019-05-10 14:09:37 -0600

    ConfigConverter - change dependency structure

--- ConfigConverter/Properties/AssemblyInfo.cs
+++ ConfigConverter/Properties/AssemblyInfo.cs
@@ -25,8 +25,8 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can use the default the Revision and 
 // Build Numbers by using the '*' as shown below:
-[assembly: AssemblyVersion("2.0.0.1")]
-[assembly: AssemblyFileVersion("2.0.0.1")]
+[assembly: AssemblyVersion("2.0.0.2")]
+[assembly: AssemblyFileVersion("2.0.0.2")]
 
 // satisfy FxCop:
 //[assembly: System.CLSCompliant(true)]
