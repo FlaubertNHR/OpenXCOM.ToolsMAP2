1bb759a2037e0067a15be05499d4a2d8ec82c729
kevL <k3v1s73n@gmail.com>
2019-08-18 06:03:07 -0600

    FileService.ReplaceFile() - create dir.
    modify message-info
    docs

--- XCom/FileService.cs
+++ XCom/FileService.cs
@@ -20,7 +20,7 @@ namespace XCom
 		{
 			using (var f = new Infobox(
 									"IO Error",
-									head + " The operation will not proceed.",
+									head, // + " The operation will not proceed.",
 									copyable))
 			{
 				f.ShowDialog();
@@ -32,7 +32,7 @@ namespace XCom
 		/// Reads all the bytes of a specified file and returns it in a buffer.
 		/// The file will be closed.
 		/// </summary>
-		/// <param name="pfe">path-file-extension</param>
+		/// <param name="pfe">path-file-extension of the file to read</param>
 		/// <returns>an array of bytes else null</returns>
 		public static byte[] ReadFile(string pfe)
 		{
@@ -47,13 +47,13 @@ namespace XCom
 				catch (Exception ex)
 				{
 					ShowDialogError(
-								"The file could not be read.",
+								"File could not be read.",
 								pfe + Environment.NewLine + Environment.NewLine + ex);
 					return null;
 				}
 			}
 			else
-				ShowDialogError("The file does not exist.", pfe);
+				ShowDialogError("File does not exist.", pfe);
 
 			return bytes;
 		}
@@ -63,7 +63,7 @@ namespace XCom
 		/// closed.
 		/// IMPORTANT: Dispose the stream in the calling function.
 		/// </summary>
-		/// <param name="pfe">path-file-extension</param>
+		/// <param name="pfe">path-file-extension of the file to be opened</param>
 		/// <returns>the filestream if valid else null</returns>
 		public static FileStream OpenFile(string pfe)
 		{
@@ -78,13 +78,13 @@ namespace XCom
 				catch (Exception ex)
 				{
 					ShowDialogError(
-								"The file could not be opened.",
+								"File could not be opened.",
 								pfe + Environment.NewLine + Environment.NewLine + ex);
 					return null;
 				}
 			}
 			else
-				ShowDialogError("The file does not exist.", pfe);
+				ShowDialogError("File does not exist.", pfe);
 
 			return fs;
 		}
@@ -94,10 +94,10 @@ namespace XCom
 		/// backing up a pre-existing file if it exists. The file will not be
 		/// closed.
 		/// IMPORTANT: Dispose the stream in the calling function.
-		/// @note Call this only to create a file_ext_[t.ext] file. Then call
-		/// ReplaceFile() by passing in file_ext.
+		/// @note If file exists call this only to create a file_ext_[t.ext]
+		/// file. Then call ReplaceFile() by passing in file_ext.
 		/// </summary>
-		/// <param name="pfe">path-file-extension</param>
+		/// <param name="pfe">path-file-extension of the file to be created</param>
 		/// <returns>the filestream if valid else null</returns>
 		public static FileStream CreateFile(string pfe)
 		{
@@ -111,7 +111,7 @@ namespace XCom
 			catch (Exception ex)
 			{
 				ShowDialogError(
-							"The file could not be created.",
+							"File could not be created.",
 							pfe + Environment.NewLine + Environment.NewLine + ex);
 				return null;
 			}
@@ -129,7 +129,7 @@ namespace XCom
 		/// file_ext.
 		/// @note The backup will be in the 'GlobalsXC.MV_Backup' subdirectory.
 		/// </summary>
-		/// <param name="pfe">path-file_extension of the file to be replaced</param>
+		/// <param name="pfe">path-file-extension of the file to be replaced</param>
 		/// <returns>true if everything goes according to plan</returns>
 		public static bool ReplaceFile(string pfe)
 		{
@@ -147,7 +147,7 @@ namespace XCom
 					catch (Exception ex)
 					{
 						ShowDialogError(
-									"The backup file could not be deleted.",
+									"File backup could not be deleted.",
 									pfeBackup + Environment.NewLine + Environment.NewLine + ex);
 						return false;
 					}
@@ -155,6 +155,7 @@ namespace XCom
 
 				try
 				{
+					Directory.CreateDirectory(dirBackup);
 					File.Replace(
 							pfe + GlobalsXC.TEMPExt,
 							pfe,
@@ -164,7 +165,7 @@ namespace XCom
 				catch (Exception ex)
 				{
 					ShowDialogError(
-								"The file could not be replaced.",
+								"File could not be replaced.",
 								pfe + Environment.NewLine + Environment.NewLine + ex);
 					return false;
 				}
@@ -178,13 +179,39 @@ namespace XCom
 				catch (Exception ex)
 				{
 					ShowDialogError(
-								"The file could not be moved.",
+								"File could not be moved.",
 								pfe + GlobalsXC.TEMPExt + Environment.NewLine + Environment.NewLine + ex);
 					return false;
 				}
 			}
 			return true;
 		}
+
+
+/*		/// <summary>
+		/// Deletes a file.
+		/// </summary>
+		/// <param name="pfe">path-file-extension of the file to be deleted</param>
+		/// <returns>true if mission was successful or file doesn't exist thus
+		/// doesn't need to be deleted</returns>
+		public static bool DeleteFile(string pfe)
+		{
+			if (File.Exists(pfe))
+			{
+				try
+				{
+					File.Delete(pfe);
+				}
+				catch (Exception ex)
+				{
+					ShowDialogError(
+								"File could not be deleted.",
+								pfe + Environment.NewLine + Environment.NewLine + ex);
+					return false;
+				}
+			}
+			return true;
+		} */
 
 /*		/// <summary>
 		/// Copies an existing file to a MapView backup directory.
