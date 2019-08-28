9f0a29fec81f9a092c86ad0abdf396d0c23bf1b1
kevL <k3v1s73n@gmail.com>
2019-08-27 21:54:24 -0600

    ProgressBarForm - tweaks

--- XCom/ProgressBarForm.cs
+++ XCom/ProgressBarForm.cs
@@ -63,8 +63,8 @@ namespace XCom
 		#region Designer
 		private System.ComponentModel.Container components = null;
 
-		private ProgressBar pbProgress;
 		private Label lblInfo;
+		private ProgressBar pbProgress;
 
 
 		/// <summary>
@@ -85,40 +85,42 @@ namespace XCom
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.pbProgress = new System.Windows.Forms.ProgressBar();
 			this.lblInfo = new System.Windows.Forms.Label();
+			this.pbProgress = new System.Windows.Forms.ProgressBar();
 			this.SuspendLayout();
-			// 
-			// pbProgress
-			// 
-			this.pbProgress.Dock = System.Windows.Forms.DockStyle.Bottom;
-			this.pbProgress.Location = new System.Drawing.Point(0, 15);
-			this.pbProgress.Name = "pbProgress";
-			this.pbProgress.Size = new System.Drawing.Size(312, 21);
-			this.pbProgress.TabIndex = 0;
 			// 
 			// lblInfo
 			// 
 			this.lblInfo.Dock = System.Windows.Forms.DockStyle.Top;
 			this.lblInfo.Location = new System.Drawing.Point(0, 0);
+			this.lblInfo.Margin = new System.Windows.Forms.Padding(0);
 			this.lblInfo.Name = "lblInfo";
-			this.lblInfo.Size = new System.Drawing.Size(312, 15);
+			this.lblInfo.Size = new System.Drawing.Size(314, 20);
 			this.lblInfo.TabIndex = 1;
 			this.lblInfo.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+			// 
+			// pbProgress
+			// 
+			this.pbProgress.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.pbProgress.Location = new System.Drawing.Point(0, 20);
+			this.pbProgress.Margin = new System.Windows.Forms.Padding(0);
+			this.pbProgress.Name = "pbProgress";
+			this.pbProgress.Size = new System.Drawing.Size(314, 19);
+			this.pbProgress.TabIndex = 0;
 			// 
 			// ProgressBarForm
 			// 
 			this.AutoScaleBaseSize = new System.Drawing.Size(5, 12);
-			this.ClientSize = new System.Drawing.Size(312, 36);
+			this.ClientSize = new System.Drawing.Size(312, 37);
 			this.ControlBox = false;
-			this.Controls.Add(this.lblInfo);
 			this.Controls.Add(this.pbProgress);
+			this.Controls.Add(this.lblInfo);
 			this.Font = new System.Drawing.Font("Verdana", 7F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
 			this.MaximizeBox = false;
-			this.MaximumSize = new System.Drawing.Size(320, 44);
+			this.MaximumSize = new System.Drawing.Size(320, 45);
 			this.MinimizeBox = false;
-			this.MinimumSize = new System.Drawing.Size(320, 44);
+			this.MinimumSize = new System.Drawing.Size(320, 45);
 			this.Name = "ProgressBarForm";
 			this.ShowIcon = false;
 			this.ShowInTaskbar = false;
