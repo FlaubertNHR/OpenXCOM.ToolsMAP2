02e8f3638ef47f815bb7a45b5ae0acaf271c7e8f
kevL <k3v1s73n@gmail.com>
2020-11-11 20:47:21 -0700

    MainViewOptionables - close PartslotTest dialog
    if base1 count options change (displayed info it no longer accurate)

--- MapView/Forms/MainView/MainViewOptionables.cs
+++ MapView/Forms/MainView/MainViewOptionables.cs
@@ -913,6 +913,10 @@ namespace MapView.Forms.MainView
 			MainViewF.that.sb_PrintPosition();
 			ObserverManager.RouteView   .Control     .PrintSelectedInfo();
 			ObserverManager.TopRouteView.ControlRoute.PrintSelectedInfo();
+
+			// close the Partslot test dialog (its displayed data has changed)
+			if (TopView._finfobox != null && !TopView._finfobox.IsDisposed)
+				TopView._finfobox.Close();
 		}
 
 		/// <summary>
