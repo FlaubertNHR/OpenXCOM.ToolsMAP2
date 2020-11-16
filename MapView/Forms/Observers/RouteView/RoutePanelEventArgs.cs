74a88267d47bb24b34d270f0ef09dd797e4564d3
kevL <k3v1s73n@gmail.com>
2020-11-15 21:58:55 -0700

    breakout RoutePanelEventArgs from RouteView.cs

---
+++ MapView/Forms/Observers/RouteView/RoutePanelEventArgs.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Windows.Forms;
+
+using XCom;
+
+
+namespace MapView.Forms.Observers
+{
+	#region Event args
+	/// <summary>
+	/// Event args for RouteView.
+	/// </summary>
+	internal sealed class RoutePanelEventArgs
+		:
+			EventArgs
+	{
+		#region Properties
+		internal MouseButtons MouseButton
+		{ get; private set; }
+
+		internal MapTile Tile
+		{ get; private set; }
+
+		internal MapLocation Location
+		{ get; private set; }
+		#endregion Properties
+
+
+		#region cTor
+		/// <summary>
+		/// cTor.
+		/// </summary>
+		/// <param name="button"></param>
+		/// <param name="tile"></param>
+		/// <param name="location"></param>
+		internal RoutePanelEventArgs(
+				MouseButtons button,
+				MapTile tile,
+				MapLocation location)
+		{
+			MouseButton = button;
+			Tile        = tile;
+			Location    = location;
+		}
+		#endregion cTor
+	}
+	#endregion Event args
+}
--- MapView/Forms/Observers/RouteView/RouteView.cs
+++ MapView/Forms/Observers/RouteView/RouteView.cs
@@ -2430,46 +2430,4 @@ namespace MapView.Forms.Observers
 		}
 		#endregion Update UI
 	}
-
-
-
-	#region Event args
-	/// <summary>
-	/// Event args for RouteView.
-	/// </summary>
-	internal sealed class RoutePanelEventArgs
-		:
-			EventArgs
-	{
-		#region Properties
-		internal MouseButtons MouseButton
-		{ get; private set; }
-
-		internal MapTile Tile
-		{ get; private set; }
-
-		internal MapLocation Location
-		{ get; private set; }
-		#endregion Properties
-
-
-		#region cTor
-		/// <summary>
-		/// cTor.
-		/// </summary>
-		/// <param name="button"></param>
-		/// <param name="tile"></param>
-		/// <param name="location"></param>
-		internal RoutePanelEventArgs(
-				MouseButtons button,
-				MapTile tile,
-				MapLocation location)
-		{
-			MouseButton = button;
-			Tile        = tile;
-			Location    = location;
-		}
-		#endregion cTor
-	}
-	#endregion Event args
 }
