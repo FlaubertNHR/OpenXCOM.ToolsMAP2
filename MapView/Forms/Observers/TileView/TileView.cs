720c5e83654c9f8a16d624261ba2c14a15126eda
kevL <k3v1s73n@gmail.com>
2020-12-01 23:00:12 -0700

    TileView.OnMcdEditClick() - explicit type & tidy

--- MapView/Forms/Observers/TileView/TileView.cs
+++ MapView/Forms/Observers/TileView/TileView.cs
@@ -491,7 +491,7 @@ namespace MapView.Forms.Observers
 		{
 			if (SelectedTilepart != null)
 			{
-				var terrain = ((MapFile)MapFile).GetTerrain(SelectedTilepart);
+				Tuple<string,string> terrain = MapFile.GetTerrain(SelectedTilepart);
 
 				string terr = terrain.Item1;
 				string path = terrain.Item2;
