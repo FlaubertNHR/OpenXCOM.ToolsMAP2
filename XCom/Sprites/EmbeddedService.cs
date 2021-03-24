f5b268ff4fdd49e676f3b99fc65a66d4609ff196
kevL <k3v1s73n@gmail.com>
2021-03-24 10:39:48 -0600

    delete MONOTONE_D.* embedded files

--- XCom/Sprites/EmbeddedService.cs
+++ XCom/Sprites/EmbeddedService.cs
@@ -17,32 +17,24 @@ namespace XCom
 		/// <returns>the monotone spriteset</returns>
 		public static Spriteset CreateMonotoneSpriteset(string label)
 		{
-			string tonetype = String.Empty;
-			if      (label == "Monotone")          tonetype = "XCom._Embedded.MONOTONE";
-			else if (label == "Monotone_crippled") tonetype = "XCom._Embedded.MONOTONE_D";
-
-			if (tonetype.Length != 0)
+			var ass = Assembly.GetExecutingAssembly();
+			using (var strPck = ass.GetManifestResourceStream("XCom._Embedded.MONOTONE" + GlobalsXC.PckExt))
+			using (var strTab = ass.GetManifestResourceStream("XCom._Embedded.MONOTONE" + GlobalsXC.TabExt))
 			{
-				var ass = Assembly.GetExecutingAssembly();
-				using (var strPck = ass.GetManifestResourceStream(tonetype + GlobalsXC.PckExt))
-				using (var strTab = ass.GetManifestResourceStream(tonetype + GlobalsXC.TabExt))
-				{
-					var bytesPck = new byte[strPck.Length];
-					var bytesTab = new byte[strTab.Length];
+				var bytesPck = new byte[strPck.Length];
+				var bytesTab = new byte[strTab.Length];
 
-					strPck.Read(bytesPck, 0, (int)strPck.Length);
-					strTab.Read(bytesTab, 0, (int)strTab.Length);
+				strPck.Read(bytesPck, 0, (int)strPck.Length);
+				strTab.Read(bytesTab, 0, (int)strTab.Length);
 
-					return new Spriteset(
-									label,
-									Palette.UfoBattle,
-									SpritesetManager.TAB_WORD_LENGTH_2,
-									bytesPck,
-									bytesTab,
-									true);
-				}
+				return new Spriteset(
+								label,
+								Palette.UfoBattle,
+								SpritesetManager.TAB_WORD_LENGTH_2,
+								bytesPck,
+								bytesTab,
+								true);
 			}
-			return null;
 		}
 	}
 }
