c0867a3cdf106e4d6208cf9a8c0a1fb8360ad259
kevL <k3v1s73n@gmail.com>
2021-10-19 23:00:10 -0600

    breakout GetArchitecture() to DateTimeExtension

--- DSShared/DateTimeExtension.cs
+++ DSShared/DateTimeExtension.cs
@@ -1,12 +1,17 @@
 ﻿using System;
 using System.IO;
 using System.Reflection;
+//#if !__MonoCS__
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+//#endif
 
 
 namespace DSShared
 {
 	/// <summary>
-	/// Determines date/time of an assembly.
+	/// Determines date/time of an assembly and/or the architecture on which the
+	/// OS is.
 	/// </summary>
 	/// <remarks>Lifted from StackOverflow.com:
 	/// https://stackoverflow.com/questions/1600962/displaying-the-build-date#answer-1600990
@@ -42,5 +47,107 @@ namespace DSShared
 
 //			return localTime;
 		}
+
+
+		// http://stackoverflow.com/questions/336633/how-to-detect-windows-64-bit-platform-with-net#answer-1840313
+		// dwhiteho -> end.
+		// w/ refactoring and tweaks to satisfy FxCop and docs. kL
+
+		public static string GetArchitecture()
+		{
+#if !__MonoCS__
+			string arch;
+			switch (Assembly.GetExecutingAssembly().GetName().ProcessorArchitecture)
+			{
+				default:
+//				case ProcessorArchitecture.None:								// An unknown or unspecified combination of processor and bits-per-word.
+					arch = "None : " + (IsOS64Bit() ? "64-bit" : "32-bit");
+					break;
+				case ProcessorArchitecture.MSIL:								// Neutral with respect to processor and bits-per-word.
+					arch = "MSIL : " + (IsOS64Bit() ? "64-bit" : "32-bit");
+					break;
+				case ProcessorArchitecture.X86:									// A 32-bit Intel processor, either native or in the
+					arch = "X86" + (IsOS64Bit() ? " : WoW64" : String.Empty);	// Windows on Windows environment on a 64-bit platform (WOW64).
+					break;
+				case ProcessorArchitecture.IA64:								// A 64-bit Intel processor only.
+					arch = "IA64";
+					break;
+				case ProcessorArchitecture.Amd64:								// A 64-bit AMD processor only.
+					arch = "Amd64";
+					break;
+			}
+			return arch;
+#else
+			return "n/a";
+#endif
+		}
+
+#if !__MonoCS__
+		/// <summary>
+		/// Checks if the user's operating system is 64-bit.
+		/// </summary>
+		/// <returns></returns>
+		private static bool IsOS64Bit()
+		{
+			return IntPtr.Size == 8
+			   || (IntPtr.Size == 4 && Is32BitProcessOn64BitProcessor());
+		}
+
+		/// <summary>
+		/// Checks if this program is running as a 32-bit process in a 64-bit
+		/// operating system.
+		/// </summary>
+		/// <returns></returns>
+		private static bool Is32BitProcessOn64BitProcessor()
+		{
+			var fnDelegate = GetIsWow64ProcessDelegate();
+			if (fnDelegate != null)
+			{
+				bool isWow64;
+				bool ret = fnDelegate.Invoke(Process.GetCurrentProcess().Handle, out isWow64);
+
+				return ret && isWow64;
+			}
+			return false;
+		}
+
+
+		private delegate bool IsWow64ProcessDelegate([In] IntPtr handle, [Out] out bool isWow64Process);
+
+		private static class NativeMethods
+		{
+			[DllImport("kernel32",
+				SetLastError = true,
+				CallingConvention = CallingConvention.Winapi,
+				BestFitMapping = false,
+				ThrowOnUnmappableChar = true)]
+			public extern static IntPtr LoadLibrary(string libraryName);
+
+			[DllImport("kernel32",
+				SetLastError = true,
+				CallingConvention = CallingConvention.Winapi,
+				BestFitMapping = false,
+				ThrowOnUnmappableChar = true)]
+			public extern static IntPtr GetProcAddress(IntPtr hwnd, string procedureName);
+		}
+
+		/// <summary>
+		/// Helper for Is32BitProcessOn64BitProcessor().
+		/// </summary>
+		/// <returns></returns>
+		private static IsWow64ProcessDelegate GetIsWow64ProcessDelegate()
+		{
+			IntPtr handle = NativeMethods.LoadLibrary("kernel32");
+			if (handle != IntPtr.Zero)
+			{
+				IntPtr fnPtr = NativeMethods.GetProcAddress(handle, "IsWow64Process");
+				if (fnPtr != IntPtr.Zero)
+					return (IsWow64ProcessDelegate)Marshal.GetDelegateForFunctionPointer(
+																					fnPtr,
+																					typeof(IsWow64ProcessDelegate));
+			}
+			return null;
+		}
+#endif
 	}
 }
--- PckView/About.Designer.cs
+++ PckView/About.Designer.cs
@@ -8,9 +8,10 @@ namespace PckView
 	{
 		#region Designer
 		private Label label1;
-		private Label lblVersion;
 		private Label label2;
-		private Label lblBuildConfig;
+		private TextBox tb_Ver;
+		private Label la_Date;
+		private Label la_Architecture;
 
 		/// <summary>
 		/// Required method for Designer support - do not modify
@@ -19,14 +20,16 @@ namespace PckView
 		private void InitializeComponent()
 		{
 			this.label1 = new System.Windows.Forms.Label();
-			this.lblVersion = new System.Windows.Forms.Label();
 			this.label2 = new System.Windows.Forms.Label();
-			this.lblBuildConfig = new System.Windows.Forms.Label();
+			this.tb_Ver = new System.Windows.Forms.TextBox();
+			this.la_Date = new System.Windows.Forms.Label();
+			this.la_Architecture = new System.Windows.Forms.Label();
 			this.SuspendLayout();
 			// 
 			// label1
 			// 
-			this.label1.Location = new System.Drawing.Point(5, 15);
+			this.label1.Location = new System.Drawing.Point(5, 5);
+			this.label1.Margin = new System.Windows.Forms.Padding(0);
 			this.label1.Name = "label1";
 			this.label1.Size = new System.Drawing.Size(285, 15);
 			this.label1.TabIndex = 0;
@@ -35,35 +38,54 @@ namespace PckView
 			// 
 			// label2
 			// 
-			this.label2.Location = new System.Drawing.Point(5, 35);
+			this.label2.Location = new System.Drawing.Point(5, 23);
+			this.label2.Margin = new System.Windows.Forms.Padding(0);
 			this.label2.Name = "label2";
 			this.label2.Size = new System.Drawing.Size(285, 15);
 			this.label2.TabIndex = 1;
 			this.label2.Text = "revised: kevL";
 			this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
 			// 
-			// lblVersion
+			// tb_Ver
 			// 
-			this.lblVersion.Location = new System.Drawing.Point(5, 65);
-			this.lblVersion.Name = "lblVersion";
-			this.lblVersion.Size = new System.Drawing.Size(285, 15);
-			this.lblVersion.TabIndex = 2;
-			this.lblVersion.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			this.tb_Ver.BorderStyle = System.Windows.Forms.BorderStyle.None;
+			this.tb_Ver.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.tb_Ver.HideSelection = false;
+			this.tb_Ver.Location = new System.Drawing.Point(5, 44);
+			this.tb_Ver.Margin = new System.Windows.Forms.Padding(0);
+			this.tb_Ver.Multiline = true;
+			this.tb_Ver.Name = "tb_Ver";
+			this.tb_Ver.ReadOnly = true;
+			this.tb_Ver.Size = new System.Drawing.Size(285, 43);
+			this.tb_Ver.TabIndex = 4;
+			this.tb_Ver.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+			this.tb_Ver.WordWrap = false;
 			// 
-			// lblBuildConfig
+			// la_Date
 			// 
-			this.lblBuildConfig.Location = new System.Drawing.Point(5, 95);
-			this.lblBuildConfig.Name = "lblBuildConfig";
-			this.lblBuildConfig.Size = new System.Drawing.Size(285, 15);
-			this.lblBuildConfig.TabIndex = 3;
-			this.lblBuildConfig.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			this.la_Date.Location = new System.Drawing.Point(5, 95);
+			this.la_Date.Margin = new System.Windows.Forms.Padding(0);
+			this.la_Date.Name = "la_Date";
+			this.la_Date.Size = new System.Drawing.Size(285, 15);
+			this.la_Date.TabIndex = 2;
+			this.la_Date.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// la_Architecture
+			// 
+			this.la_Architecture.Location = new System.Drawing.Point(5, 113);
+			this.la_Architecture.Margin = new System.Windows.Forms.Padding(0);
+			this.la_Architecture.Name = "la_Architecture";
+			this.la_Architecture.Size = new System.Drawing.Size(285, 15);
+			this.la_Architecture.TabIndex = 3;
+			this.la_Architecture.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
 			// 
 			// About
 			// 
 			this.AutoScaleBaseSize = new System.Drawing.Size(5, 12);
-			this.ClientSize = new System.Drawing.Size(294, 126);
-			this.Controls.Add(this.lblBuildConfig);
-			this.Controls.Add(this.lblVersion);
+			this.ClientSize = new System.Drawing.Size(294, 134);
+			this.Controls.Add(this.tb_Ver);
+			this.Controls.Add(this.la_Architecture);
+			this.Controls.Add(this.la_Date);
 			this.Controls.Add(this.label2);
 			this.Controls.Add(this.label1);
 			this.Font = new System.Drawing.Font("Verdana", 7F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
@@ -72,8 +94,9 @@ namespace PckView
 			this.MinimizeBox = false;
 			this.Name = "About";
 			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
-			this.Text = "About";
+			this.Text = "PckView about";
 			this.ResumeLayout(false);
+			this.PerformLayout();
 
 		}
 		#endregion Designer
