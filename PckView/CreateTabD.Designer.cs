0e0c704bbaba847d17505f1e1a4918a60c4c78c3
kevL <k3v1s73n@gmail.com>
2022-05-06 00:58:13 -0600

    CreateTabD - layout tweaks

--- PckView/CreateTabD.Designer.cs
+++ PckView/CreateTabD.Designer.cs
@@ -38,8 +38,8 @@ namespace PckView
 			this.la_head.Location = new System.Drawing.Point(0, 0);
 			this.la_head.Margin = new System.Windows.Forms.Padding(0);
 			this.la_head.Name = "la_head";
-			this.la_head.Padding = new System.Windows.Forms.Padding(2, 3, 0, 0);
-			this.la_head.Size = new System.Drawing.Size(397, 20);
+			this.la_head.Padding = new System.Windows.Forms.Padding(3, 4, 0, 0);
+			this.la_head.Size = new System.Drawing.Size(402, 20);
 			this.la_head.TabIndex = 0;
 			this.la_head.Text = "Use this tool to create a TAB file from a PCK (compressed) data file.";
 			// 
@@ -48,10 +48,10 @@ namespace PckView
 			this.tb_input.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
 			| System.Windows.Forms.AnchorStyles.Right)));
 			this.tb_input.HideSelection = false;
-			this.tb_input.Location = new System.Drawing.Point(4, 23);
+			this.tb_input.Location = new System.Drawing.Point(1, 22);
 			this.tb_input.Margin = new System.Windows.Forms.Padding(0);
 			this.tb_input.Name = "tb_input";
-			this.tb_input.Size = new System.Drawing.Size(363, 19);
+			this.tb_input.Size = new System.Drawing.Size(377, 19);
 			this.tb_input.TabIndex = 1;
 			this.tb_input.WordWrap = false;
 			this.tb_input.TextChanged += new System.EventHandler(this.OnInputTextchanged);
@@ -59,7 +59,7 @@ namespace PckView
 			// bu_open
 			// 
 			this.bu_open.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-			this.bu_open.Location = new System.Drawing.Point(369, 22);
+			this.bu_open.Location = new System.Drawing.Point(377, 21);
 			this.bu_open.Margin = new System.Windows.Forms.Padding(0);
 			this.bu_open.Name = "bu_open";
 			this.bu_open.Size = new System.Drawing.Size(25, 21);
@@ -70,7 +70,7 @@ namespace PckView
 			// 
 			// rb_2byte
 			// 
-			this.rb_2byte.Location = new System.Drawing.Point(10, 48);
+			this.rb_2byte.Location = new System.Drawing.Point(10, 43);
 			this.rb_2byte.Margin = new System.Windows.Forms.Padding(0);
 			this.rb_2byte.Name = "rb_2byte";
 			this.rb_2byte.Size = new System.Drawing.Size(100, 20);
@@ -81,7 +81,7 @@ namespace PckView
 			// 
 			// rb_4byte
 			// 
-			this.rb_4byte.Location = new System.Drawing.Point(10, 68);
+			this.rb_4byte.Location = new System.Drawing.Point(10, 63);
 			this.rb_4byte.Margin = new System.Windows.Forms.Padding(0);
 			this.rb_4byte.Name = "rb_4byte";
 			this.rb_4byte.Size = new System.Drawing.Size(100, 20);
@@ -94,10 +94,10 @@ namespace PckView
 			this.la_warn.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
 			| System.Windows.Forms.AnchorStyles.Right)));
 			this.la_warn.ForeColor = System.Drawing.Color.DarkSalmon;
-			this.la_warn.Location = new System.Drawing.Point(0, 88);
+			this.la_warn.Location = new System.Drawing.Point(0, 82);
 			this.la_warn.Margin = new System.Windows.Forms.Padding(0);
 			this.la_warn.Name = "la_warn";
-			this.la_warn.Size = new System.Drawing.Size(397, 15);
+			this.la_warn.Size = new System.Drawing.Size(402, 15);
 			this.la_warn.TabIndex = 5;
 			this.la_warn.Text = "This is not guaranteed to be valid.";
 			this.la_warn.TextAlign = System.Drawing.ContentAlignment.TopCenter;
@@ -106,7 +106,7 @@ namespace PckView
 			// 
 			this.bu_create.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
 			this.bu_create.Enabled = false;
-			this.bu_create.Location = new System.Drawing.Point(9, 104);
+			this.bu_create.Location = new System.Drawing.Point(11, 98);
 			this.bu_create.Margin = new System.Windows.Forms.Padding(0);
 			this.bu_create.Name = "bu_create";
 			this.bu_create.Size = new System.Drawing.Size(100, 21);
@@ -119,7 +119,7 @@ namespace PckView
 			// 
 			this.bu_cancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
 			this.bu_cancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-			this.bu_cancel.Location = new System.Drawing.Point(313, 104);
+			this.bu_cancel.Location = new System.Drawing.Point(316, 98);
 			this.bu_cancel.Margin = new System.Windows.Forms.Padding(0);
 			this.bu_cancel.Name = "bu_cancel";
 			this.bu_cancel.Size = new System.Drawing.Size(75, 21);
@@ -131,7 +131,7 @@ namespace PckView
 			// 
 			this.AcceptButton = this.bu_create;
 			this.CancelButton = this.bu_cancel;
-			this.ClientSize = new System.Drawing.Size(397, 128);
+			this.ClientSize = new System.Drawing.Size(402, 122);
 			this.Controls.Add(this.la_warn);
 			this.Controls.Add(this.rb_4byte);
 			this.Controls.Add(this.rb_2byte);
