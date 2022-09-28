f3da8483151aa2ae893f19127973360229f0815d
kevL <k3v1s73n@gmail.com>
2022-09-28 14:30:15 -0600

    Mapview 4.4.2.0 - update ver and date

--- MapView/Properties/AssemblyInfo.cs
+++ MapView/Properties/AssemblyInfo.cs
@@ -31,9 +31,9 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values, or the defaults by using '*' for the Build
 // and Revision.
-[assembly: AssemblyVersion("4.4.1.0")]
-[assembly: AssemblyFileVersion("4.4.1.0")]
-[assembly: AssemblyInformationalVersion("4.4.1.0")]
+[assembly: AssemblyVersion("4.4.2.0")]
+[assembly: AssemblyFileVersion("4.4.2.0")]
+[assembly: AssemblyInformationalVersion("4.4.2.0")]
 
 // satisfy FxCop:
 //[assembly: System.CLSCompliant(true)]
