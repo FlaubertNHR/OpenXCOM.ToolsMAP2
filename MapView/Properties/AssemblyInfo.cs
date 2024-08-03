bd8b178d63d052eede9eb6e0f13dc6f2e2fbd841
kevL <k3v1s73n@gmail.com>
2024-08-03 05:44:04 -0600

    update ver and date

--- MapView/Properties/AssemblyInfo.cs
+++ MapView/Properties/AssemblyInfo.cs
@@ -32,9 +32,9 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values, or the defaults by using '*' for the Build
 // and Revision.
-[assembly: AssemblyVersion("4.5.0.0")]
-[assembly: AssemblyFileVersion("4.5.0.0")]
-[assembly: AssemblyInformationalVersion("4.5.0.0")]
+[assembly: AssemblyVersion("4.6.0.0")]
+[assembly: AssemblyFileVersion("4.6.0.0")]
+[assembly: AssemblyInformationalVersion("4.6.0.0")]
 
 // satisfy FxCop:
 //[assembly: System.CLSCompliant(true)]
